1efb3d685cdc56bb6a7ee161652bace3efc4a405
Somogyi Botond <somogyi.botond.munka@gmail.com>
2023-04-13 11:10:35 +0200

    singleton

--- src/main/java/com/github/martinnagy/javaoptimizationexercise/SingletonExercise1.java
+++ src/main/java/com/github/martinnagy/javaoptimizationexercise/SingletonExercise1.java
@@ -6,15 +6,18 @@ public class SingletonExercise1 {
     public static void main(String[] args) {
         StopWatch stopWatch = StopWatch.createStarted();
         for (int i = 0; i < 500_000_000; i++) {
-            OptimizeMe.getInstance();
+            OptimizeMe optimizeMe = OptimizeMe.getInstance();
         }
         System.out.println(stopWatch);
     }
 
     private static class OptimizeMe {
-        private static OptimizeMe instance;
+        private static OptimizeMe instance = null;
 
         static OptimizeMe getInstance() {
+            if (instance != null) {
+                return instance;
+            }
             synchronized (OptimizeMe.class) {
                 if (instance == null) {
                     instance = new OptimizeMe();
